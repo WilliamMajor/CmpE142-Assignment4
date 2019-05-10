7fa0aceec844475ab747e89f09eeb7c34db2ab1d
WilliamMajor <epiphone1995@gmail.com>
2019-05-09 23:25:09 -0700

    More setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,22 @@
 
 using namespace std;
 
-void *customer1(void *threadid);
-void *customer2(void *threadid);
-void *customer3(void *threadid);
+void *customerH(void *threadid);
+void *customerF(void *threadid);
+void *customerS(void *threadid);
 void *chef(void *threadid);
 
 int main(int argc, char *argv[]) {
 	
 	pthread_t p1, p2, p3, p4;
-	int i, rc;
+    //use an int array to handle the current food being served 
+	int i[2];
+    int rc;
 	cout << "start of main" << endl;
-	rc = pthread_create(&p1, NULL, &customer1, NULL);
-	rc = pthread_create(&p2, NULL, &customer2, NULL);
-	rc = pthread_create(&p3, NULL, &customer3, NULL );
-	rc = pthread_create(&p4, NULL, &chef, NULL );
+	rc = pthread_create(&p1, NULL, &customer1, (void *)i);
+	rc = pthread_create(&p2, NULL, &customer2, (void *)i);
+	rc = pthread_create(&p3, NULL, &customer3, (void *)i);
+	rc = pthread_create(&p4, NULL, &chef, (void *)i);
 
 	pthread_join(p1,NULL);
 	pthread_join(p2,NULL);
@@ -29,18 +31,27 @@ int main(int argc, char *argv[]) {
 	cout << "end of main" << endl;
 
 }
-void *customer1(void *threadid)
+void *customerH(void *threadid)
 {
+    bool hasHamburger = true;
+    bool hasFries = false;
+    bool hasSoda = false;
 	cout << "customer 1" << endl;
 	pthread_exit(NULL);
 }
-void *customer2(void *threadid)
+void *customerF(void *threadid)
 {
+    bool hasHamburger = false;
+    bool hasFries = true;
+    bool hasSoda = false;
 	cout << "customer 2" << endl;
 	pthread_exit(NULL);
 }
-void *customer3(void *threadid)
+void *customerS(void *threadid)
 {
+    bool hasHamburger = true;
+    bool hasFries = false;
+    bool hasSoda = true;
 	cout << "customer 3" << endl;
 	pthread_exit(NULL);
 }
